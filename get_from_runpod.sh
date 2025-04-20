0b1bbe8eb97321d3ef42f8f48e7a3ee24a3444ac
Adbhut <adbhut.bhardwaj1410@gmail.com>
2025-04-20 18:46:27 +0530

    Remove extra files

--- get_from_runpod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RUNPOD_HOST=$1
-RUNPOD_PORT=$2
-RUNPOD_PROJECT_NAME=$3
-
-mkdir -p ./full_fine_tuning/model
-scp -P ${RUNPOD_PORT} -r root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}/full_fine_tuning/model/* ./full_fine_tuning/model/
